5e473c0b4fdbb1962598023f3d3c4ba9fd36b92d
sampetchey <sam_petchey@yahoo.com>
2022-09-24 07:24:54 +0000

    media query layout adjustment for smaller screens

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// page loads - initial settings
+
+// player clicks play - play button remains dark/ until end of game
+
+// audio file is randomly selected from assets/audio/30_cent, sharp and flat folders
+
+// audio file plays through MP3 player
+
+//     onclick repeat button the same MP3 file is repeated, and number of times 
+
+// player selects sharp or flat
+
+// CORRECT! - if selection matches the folder - 'sharp' or 'flat':
+
+//     ladder img transitions bottom to top
+
+//     score increases by 1
+
+//     new randomly selected audio file plays from 30 cent folder
+
+//     process repeats following this graduation of difficulty:
+
+//         2 x 30_cent-21_cent folders
+//         5 x 20_cent-11_cent folders
+//         3 x 10_cent-1_cent folders
+
+//         maximum possible score is 100
+
+//     cent level is displayed
+
+// INCORRECT! - if player selection does not match the audio file folder - 'sharp' or 'flat'
+
+//     lightbox appears
+
+//         'incorrect'
+//         the correct answer was ...
+//         repeat button to listen
+//         display total score...
+//         play again button
+//         quit button
+
+// MP3 player
+
